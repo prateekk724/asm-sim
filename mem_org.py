d19f5930deff0a1ba3a32b7b991f6486e08969ff
Anubhav\-Patel <anubhavpatel099@gmail.com>
2022-06-24 17:05:43 +0530

    extended memory organization calculation types

--- mem_org.py
+++ mem_org.py
@@ -15,26 +15,52 @@ def getBytes(mem_size):
         case 'M'|'m' : exp = 2
         case 'G'|'g' : exp = 3
     match size_factor[1]:
-        case 'b': mul = 8
+        case 'b': mul = 0.125
         case 'B': mul = 1
     return mem_bytes * (1024)**exp * mul
+
+def getMemSize(mem_bytes):
+    scaleFactor = 0
+    units = ["B", "KB", "MB", "GB", "TB", "PB"]
+    while(mem_bytes > 1024.0 and scaleFactor < 5):
+        mem_bytes = mem_bytes / 1024
+        scaleFactor = scaleFactor + 1
+    return [str(mem_bytes), units[scaleFactor]]
 
 # instruction formats.
 # Type A: <Q bit opcode><-------P bit address--------><X bit register>
 # Type B: <Q bit opcode><R bit filler><X bit register><X bit register>
+def typeA():
+    memorySize = input("Memory size (e.g. 16 GB) : ").split()
+    addressableType = input("Addressable type : ")
+    instructionLength = int(input("Instruction length (bits) : "))
+    registerLength = int(input("Register length (bits) : "))
 
-memorySize = input("Memory size (e.g. 16 GB) : ").split()
-addressableType = input("Addressable type : ")
-instructionLength = int(input("Instruction length (bits) : "))
-registerLength = int(input("Register length (bits) : "))
+    memoryBytes = getBytes(memorySize)
+    memoryAddressLength = ceil(log2(memoryBytes))
+    opcodeSize = instructionLength - (memoryAddressLength + registerLength)
+    fillerSize = instructionLength - (opcodeSize + (2 * registerLength))
+    print("="*25)
+    print("Minimum bits needed to represent an address : ", memoryAddressLength)
+    print("Bits needed for opcode : ", opcodeSize)
+    print("Filler bits in type 2 instruction : ", fillerSize)
+    print("Maximum number of instructions supported by ISA : ", 2**opcodeSize)
+    print("Maximum number of registers supported by ISA : ", 2**registerLength)
 
-memoryBytes = getBytes(memorySize)
-memoryAddressLength = ceil(log2(memoryBytes))
-opcodeSize = instructionLength - (memoryAddressLength + registerLength)
-fillerSize = instructionLength - (opcodeSize + (2 * registerLength))
-print("="*25)
-print("Minimum bits needed to represent an address : ", memoryAddressLength)
-print("Bits needed for opcode : ", opcodeSize)
-print("Filler bits in type 2 instruction : ", fillerSize)
-print("Maxiumum number of instructions supported by ISA : ", 2**opcodeSize)
-print("Maxiumum number of registers supported by ISA : ", 2**registerLength)
+def typeB1():
+    memorySize = input("Memory size (e.g. 16 GB) : ").split()
+    addressableType = input("Addressable type : ")
+    cpuBits = int(input("CPU bits : "))
+    newAddressableType = input("New Addressable type : ")
+    supportedAddrTypes = {"bit" : 1, "nibble" : 4, "byte" : 8, "word" : cpuBits}
+    memoryBits = getBytes(memorySize) * 8 
+    scaleFactor = supportedAddrTypes[addressableType] / supportedAddrTypes[newAddressableType]
+    return ceil(log2(scaleFactor))
+
+def typeB2():
+    cpuBits = int(input("CPU bits : "))
+    addressPins = int(input("Number of address Pins : "))
+    addressableType = input("Addressable type : ")
+    supportedAddrTypes = {"bit" : 1, "nibble" : 4, "byte" : 8, "word" : cpuBits}
+    memorySize = 2**addressPins * supportedAddrTypes[addressableType] * 0.125
+    return getMemSize(memorySize)
