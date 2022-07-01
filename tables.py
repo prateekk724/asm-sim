c888c28c4c32e62ab055801126057cd1b9a95224
Prateek Kumar <prateekk724@gmail.com>
2022-07-01 13:41:06 +0530

    updated tables

--- tables.py
+++ tables.py
@@ -1,25 +1,25 @@
 opcodes = {
 #   opcode  : instrxn   type    syntax                  semantic
-    '10000' : 'add',    # A     add reg1 reg2 reg3      reg3 = reg1 + reg2
-    '10001' : 'sub',    # A     sub reg1 reg2 reg3      reg3 = reg1 - reg2
-    '10010' : 'mov',    # B     mov reg1 $Imm           reg1 = $Imm
-    '10011' : 'mov',    # C     mov reg1 reg2           reg2 = reg1
-    '10100' : 'ld',     # D     ld reg1 mem_addr        reg1 = [mem_addr]
-    '10101' : 'st',     # D     st reg1 mem_addr        [mem_addr] = reg1
-    '10110' : 'mul',    # A     mul reg1 reg2 reg3      reg3 = reg1 * reg2
-    '10111' : 'div',    # C     div reg3 reg4           R0 = reg3 / reg4, R1 = reg3 % reg4
-    '11000' : 'rs',     # B     rs reg1 $Imm            reg1 = reg1 >> $Imm
-    '11001' : 'ls',     # B     ls reg1 $Imm            reg1 = reg1 << $Imm
-    '11010' : 'xor',    # A     xor reg1 reg2 reg3      reg3 = reg1 XOR reg2
-    '11011' : 'or',     # A     or reg1 reg2 reg3       reg3 = reg1 OR reg2
-    '11100' : 'and',    # A     and reg1 reg2 reg3      reg3 = reg1 AND reg2
-    '11101' : 'not',    # C     not reg1 reg2           reg2 = NOT reg1
-    '11110' : 'cmp',    # C     cmp reg1 reg2           FLAGS = reg1 CMP reg2
-    '11111' : 'jmp',    # E     jmp mem_addr
-    '01100' : 'jlt',    # E     jlt mem_addr
-    '01101' : 'jgt',    # E     jgt mem_addr
-    '01111' : 'je',     # E     je mem_addr
-    '01010' : 'hlt'     # F     hlt
+    'add' : '10000',    # A     add reg1 reg2 reg3      reg3 = reg1 + reg2
+    'sub' : '10001',    # A     sub reg1 reg2 reg3      reg3 = reg1 - reg2
+    'mov' : '10010',    # B     mov reg1 $Imm           reg1 = $Imm
+    'mov' : '10011',    # C     mov reg1 reg2           reg2 = reg1
+    'ld'  : '10100',    # D     ld reg1 mem_addr        reg1 = [mem_addr]
+    'st'  : '10101',    # D     st reg1 mem_addr        [mem_addr] = reg1
+    'mul' : '10110',    # A     mul reg1 reg2 reg3      reg3 = reg1 * reg2
+    'div' : '10111',    # C     div reg3 reg4           R0 = reg3 / reg4, R1 = reg3 % reg4
+    'rs'  : '11000',    # B     rs reg1 $Imm            reg1 = reg1 >> $Imm
+    'ls'  : '11001',    # B     ls reg1 $Imm            reg1 = reg1 << $Imm
+    'xor' : '11010',    # A     xor reg1 reg2 reg3      reg3 = reg1 XOR reg2
+    'or'  : '11011',    # A     or reg1 reg2 reg3       reg3 = reg1 OR reg2
+    'and' : '11100',    # A     and reg1 reg2 reg3      reg3 = reg1 AND reg2
+    'not' : '11101',    # C     not reg1 reg2           reg2 = NOT reg1
+    'cmp' : '11110',    # C     cmp reg1 reg2           FLAGS = reg1 CMP reg2
+    'jmp' : '11111',    # E     jmp mem_addr
+    'jlt' : '01100',    # E     jlt mem_addr
+    'jgt' : '01101',    # E     jgt mem_addr
+    'je'  : '01111',    # E     je mem_addr
+    'hlt' : '01010'     # F     hlt
 }
 
 instructionType = {
@@ -32,14 +32,14 @@ instructionType = {
 
 registers = {
 #   addr  : name
-    '000' : 'R0',
-    '001' : 'R1',
-    '010' : 'R2',
-    '011' : 'R3',
-    '100' : 'R4',
-    '101' : 'R5',
-    '110' : 'R6',
-    '111' : 'FLAGS'
+    'R0' : '000',
+    'R1' : '001',
+    'R2' : '010',
+    'R3' : '011',
+    'R4' : '100',
+    'R5' : '101',
+    'R6' : '110',
+#   '111' : 'FLAGS'
 }
 
 errors = {
